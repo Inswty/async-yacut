6337dab59300d3d987aed2280d2956e1b2c62a56
Pavel Kulichenko <pavel.kulichenko@gmail.com>
2025-08-18 13:00:07 +0500

    cut is ready

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,20 +1,23 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired, FileAllowed, MultipleFileField
-from wtforms import URLField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms import StringField, URLField
+from wtforms.validators import DataRequired, Length, Optional, Regexp
 
 from .constants import MAX_ORIGINAL_LINK_LENGTH, MAX_SHORT_LINK_LENGTH
 
 
-class IndexForm(FlaskForm):
+class LinkForm(FlaskForm):
     original_link = URLField(
         'Добавьте оригинальную длинную ссылку',
         validators=[DataRequired(message='Обязательное поле'),
                     Length(1, MAX_ORIGINAL_LINK_LENGTH)]
     )
-    custom_id = URLField(
+    custom_id = StringField(
         'Ваш вариант короткого идентификатора',
-        validators=[Length(1, MAX_SHORT_LINK_LENGTH), Optional()]
+        validators=[
+            Length(1, MAX_SHORT_LINK_LENGTH), Optional(),
+            Regexp(r'^[a-zA-Z0-9]+$', message="Можно только латинские буквы и цифры")
+        ]
     )
 
 
@@ -22,8 +25,6 @@ class UploadFileForm(FlaskForm):
     files = MultipleFileField(
         'Выберите файлы',
         validators=[
-            FileRequired(message="Файл обязателен для загрузки"),
-            FileAllowed(['jpg', 'jpeg', 'png', 'gif'],
-                        message='Разрешены только файлы: jpg, jpeg, png, gif')
+            FileRequired(message="Файл обязателен для загрузки")
         ]
     )
--- yacut/views.py
+++ yacut/views.py
@@ -1,13 +1,38 @@
-from flask import abort, flash, redirect, render_template, url_for
+from flask import abort, flash, redirect, render_template, request, url_for
 
 
 from . import app, db
-from .forms import UploadFileForm
+from .forms import LinkForm, UploadFileForm
+from .models import URLMap
+from .services import get_unique_short_id
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index_view():
-    return render_template('main.html')
+    form = LinkForm()
+    if form.validate_on_submit():
+        if form.custom_id.data:
+            short_link = form.custom_id.data
+            if (
+                URLMap.query.filter_by(short=short_link).first() or
+                short_link == 'files'
+            ):
+                flash("Предложенный вариант короткой ссылки уже существует.",
+                      "error")
+                return render_template("main.html", form=form)
+        else:
+            while True:
+                short_link = get_unique_short_id()
+                if not URLMap.query.filter_by(short=short_link).first():
+                    break
+        urlmap = URLMap(
+            original=form.original_link.data,
+            short=short_link
+        )
+        db.session.add(urlmap)
+        db.session.commit()
+        return render_template('main.html', form=form, short_link=short_link)
+    return render_template('main.html', form=form)
 
 
 @app.route('/files')
@@ -16,5 +41,9 @@ def upload_view():
     return render_template('upload.html', form=form)
 
 
-def revers():
-    pass
+@app.route('/<string:short>')
+def follow_link(short):
+    urlmap = URLMap.query.filter_by(short=short).first()
+    if urlmap is None:
+        abort(404)
+    return redirect(urlmap.original)
